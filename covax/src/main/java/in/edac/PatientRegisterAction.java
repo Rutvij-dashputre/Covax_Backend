44640e7783a96d866340f641de0dca8b62bd6eee
your_username <you@example.com>
2021-02-26 20:58:06 +0530

    second version login

--- covax/src/main/java/in/edac/PatientRegisterAction.java
+++ covax/src/main/java/in/edac/PatientRegisterAction.java
@@ -3,17 +3,18 @@ package in.edac;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
-@RestController
+@Controller
 @RequestMapping("/patient-register")
 public class PatientRegisterAction {
 	
@@ -26,7 +27,7 @@ public class PatientRegisterAction {
 	}
 	
 	
-	@GetMapping("/")
+	@GetMapping("/show")
 	public List<Patient> getPatient() {
 		return patientRepository.findAll();
 	}
@@ -47,11 +48,30 @@ public class PatientRegisterAction {
 	public void deletePatient(@PathVariable int id) {
 		patientRepository.deleteById(id);
 	}
+	
+	@GetMapping("/login")
+	public ModelAndView log() {
+		ModelAndView mv=new ModelAndView("login");
+		return mv;
+	}
+	
+	
+	@GetMapping("/authPtlogin")
+	public ModelAndView Authenticatepatient(String username,String password) {	
+		Patient patient=patientRepository.findByUsernameAndPassword(username, password);
+		if(patient!=null) {
+			ModelAndView mv=new ModelAndView("login");
+			mv.addObject("patient", patient);
+			return mv;
+		}		
+		else {
+			ModelAndView mv=new ModelAndView("Appointment");
+			mv.addObject("ptLogFail", 0);
+			return mv;
+		}
+	}
 
 
-	
-	
-	
 }
 	
 	
--- covax/src/main/java/in/edac/PatientRepository.java
+++ covax/src/main/java/in/edac/PatientRepository.java
@@ -1,8 +1,16 @@
 package in.edac;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
-	
+	Patient findByUsernameAndPassword(String username, String password);
+	/*
+	 * List<User> findByUsername(String username);
+	 * 
+	 * User findByUsernameAndPassword(String username, String password);
+	 * 
+	 * List<User> findByUsernameOrEmail(String username, String email);
+	 */
 }
